bb86c1ccbe72836656f63970c16ceb212cdce2ab
alejandro <nestoraca.179@gmail.com>
2022-08-22 23:01:23 -0400

    otros detalles

--- ProfitTM/Areas/Ventas/Controllers/RepsController.cs
+++ ProfitTM/Areas/Ventas/Controllers/RepsController.cs
@@ -12,14 +12,17 @@ namespace ProfitTM.Areas.Ventas.Controllers
 {
     public class RepsController : Controller
     {
+        private readonly static DateTime fecha_h = DateTime.Now;
+        private readonly static DateTime fecha_d = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+
         // RepFacturaVentaxArt
         RepFacturaVentaxArt report = new RepFacturaVentaxArt();
         public ActionResult RepFacturaVentaxArtPartial()
         {
             string connect = Session["connect"].ToString();
             report.PB_Logo.ImageUrl = Request.Url.Scheme + "://" + Request.Url.Authority + "/images/Logo-prod.png";
-            report.Parameters["fecDesde"].Value = new DateTime(2000, 01, 01);
-            report.Parameters["fecHasta"].Value = DateTime.Now;
+            report.Parameters["fecDesde"].Value = fecha_d;
+            report.Parameters["fecHasta"].Value = fecha_h;
 
             SqlDataSource ds = report.DataSource as SqlDataSource;
             ds.Connection.ConnectionString = "XpoProvider=MSSqlServer;" + connect;
@@ -38,8 +41,8 @@ namespace ProfitTM.Areas.Ventas.Controllers
         {
             string connect = Session["connect"].ToString();
             report1.PB_Logo.ImageUrl = Request.Url.Scheme + "://" + Request.Url.Authority + "/images/Logo-prod.png";
-            report1.Parameters["fecDesde"].Value = new DateTime(2000, 01, 01);
-            report1.Parameters["fecHasta"].Value = DateTime.Now;
+            report1.Parameters["fecDesde"].Value = fecha_d;
+            report1.Parameters["fecHasta"].Value = fecha_h;
 
             SqlDataSource ds = report1.DataSource as SqlDataSource;
             ds.Connection.ConnectionString = "XpoProvider=MSSqlServer;" + connect;
@@ -58,8 +61,8 @@ namespace ProfitTM.Areas.Ventas.Controllers
         {
             string connect = Session["connect"].ToString();
             report2.PB_Logo.ImageUrl = Request.Url.Scheme + "://" + Request.Url.Authority + "/images/Logo-prod.png";
-            report2.Parameters["fecDesde"].Value = new DateTime(2000, 01, 01);
-            report2.Parameters["fecHasta"].Value = DateTime.Now;
+            report2.Parameters["fecDesde"].Value = fecha_d;
+            report2.Parameters["fecHasta"].Value = fecha_h;
 
             SqlDataSource ds = report2.DataSource as SqlDataSource;
             ds.Connection.ConnectionString = "XpoProvider=MSSqlServer;" + connect;
@@ -89,95 +92,6 @@ namespace ProfitTM.Areas.Ventas.Controllers
         public ActionResult RepFormatoFacturaVentaPartialExport()
         {
             return DocumentViewerExtension.ExportTo(report3, Request);
-        }
-
-        // GRIDVIEW
-        [ValidateInput(false)]
-        public ActionResult GridViewFactRengsPartial(string doc_num)
-        {
-            string connect = Session["connect"].ToString();
-            EntityConnectionStringBuilder entity = EntityController.GetEntity(connect);
-            ProfitAdmEntities db = new ProfitAdmEntities(entity.ToString());
-
-            List<saFacturaVentaReng> model = db.saFacturaVentaReng.Where(r => r.doc_num == doc_num).ToList();
-            return PartialView("~/Areas/Ventas/Views/Procesos/_GridViewFactRengsPartial.cshtml", model);
-        }
-
-        [HttpPost, ValidateInput(false)]
-        public ActionResult GridViewFactRengsPartialAddNew(saFacturaVentaReng item)
-        {
-            string connect = Session["connect"].ToString();
-            EntityConnectionStringBuilder entity = EntityController.GetEntity(connect);
-            ProfitAdmEntities db = new ProfitAdmEntities(entity.ToString());
-
-            List<saFacturaVentaReng> model = db.saFacturaVentaReng.ToList();
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    model.Add(item);
-                    db.SaveChanges();
-                }
-                catch (Exception e)
-                {
-                    ViewData["EditError"] = e.Message;
-                }
-            }
-            else
-                ViewData["EditError"] = "Please, correct all errors.";
-            return PartialView("~/Areas/Ventas/Views/Procesos/_GridViewFactRengsPartial.cshtml", model.ToList());
-        }
-        [HttpPost, ValidateInput(false)]
-        public ActionResult GridViewFactRengsPartialUpdate(saFacturaVentaReng item)
-        {
-            string connect = Session["connect"].ToString();
-            EntityConnectionStringBuilder entity = EntityController.GetEntity(connect);
-            ProfitAdmEntities db = new ProfitAdmEntities(entity.ToString());
-
-            List<saFacturaVentaReng> model = db.saFacturaVentaReng.ToList();
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    var modelItem = model.FirstOrDefault(it => it.reng_num == item.reng_num);
-                    if (modelItem != null)
-                    {
-                        this.UpdateModel(modelItem);
-                        db.SaveChanges();
-                    }
-                }
-                catch (Exception e)
-                {
-                    ViewData["EditError"] = e.Message;
-                }
-            }
-            else
-                ViewData["EditError"] = "Please, correct all errors.";
-            return PartialView("~/Areas/Ventas/Views/Procesos/_GridViewFactRengsPartial.cshtml", model.ToList());
-        }
-        [HttpPost, ValidateInput(false)]
-        public ActionResult GridViewFactRengsPartialDelete(Int32 reng_num)
-        {
-            string connect = Session["connect"].ToString();
-            EntityConnectionStringBuilder entity = EntityController.GetEntity(connect);
-            ProfitAdmEntities db = new ProfitAdmEntities(entity.ToString());
-
-            List<saFacturaVentaReng> model = db.saFacturaVentaReng.ToList();
-            if (reng_num >= 0)
-            {
-                try
-                {
-                    var item = model.FirstOrDefault(it => it.reng_num == reng_num);
-                    if (item != null)
-                        model.Remove(item);
-                    db.SaveChanges();
-                }
-                catch (Exception e)
-                {
-                    ViewData["EditError"] = e.Message;
-                }
-            }
-            return PartialView("~/Areas/Ventas/Views/Procesos/_GridViewFactRengsPartial.cshtml", model.ToList());
         }
     }
 }
