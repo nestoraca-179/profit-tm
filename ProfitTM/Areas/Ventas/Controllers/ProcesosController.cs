fd8946367d3c98df8578d210d27a9b36f964dd57
Nestor A. Carreno A <it-002@lan.grupoish.net.ve>
2022-12-02 15:54:07 -0400

    interfaz de cajas

--- ProfitTM/Areas/Ventas/Controllers/ProcesosController.cs
+++ ProfitTM/Areas/Ventas/Controllers/ProcesosController.cs
@@ -132,5 +132,30 @@ namespace ProfitTM.Areas.Ventas.Controllers
                 return View();
             }
         }
+    
+        public ActionResult Caja()
+        {
+            ViewBag.user = Session["USER"];
+            ViewBag.connect = Session["CONNECT"];
+            ViewBag.modules = Session["MODULES"];
+            ViewBag.product = "Administrativo";
+
+            if (ViewBag.user == null)
+            {
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Index", "Home", new { area = "", message = "Debes iniciar sesión" });
+            }
+            else if (ViewBag.connect == null)
+            {
+                return RedirectToAction("Logout", "Account", new { area = "", msg = "Debes elegir una empresa" });
+            }
+            else
+            {
+                ViewBag.data_conn = Session["DATA_CONN"].ToString();
+                ViewBag.bran_conn = Session["BRAN_CONN"].ToString();
+
+                return View();
+            }
+        }
     }
 }
