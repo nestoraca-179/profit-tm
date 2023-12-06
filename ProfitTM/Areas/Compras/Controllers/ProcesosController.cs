e2bfa30c67224171150534804f7fb1a13146dc8d
Alejandro Carreno <nestoraca.179@gmail.com>
2023-12-06 16:58:26 -0400

    pagina orden de compra

--- ProfitTM/Areas/Compras/Controllers/ProcesosController.cs
+++ ProfitTM/Areas/Compras/Controllers/ProcesosController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Web.Security;
+using ProfitTM.Models;
 
 namespace ProfitTM.Areas.Compras.Controllers
 {
@@ -12,6 +15,41 @@ namespace ProfitTM.Areas.Compras.Controllers
             ViewBag.bran_conn = Session["BRAN_CONN"].ToString();
 
             return View();
+        }
+
+        public ActionResult OrdenCompra()
+        {
+            ViewBag.user = Session["USER"];
+            ViewBag.connect = Session["CONNECT"];
+            ViewBag.modules = Session["MODULES"];
+            ViewBag.product = "Administrativo";
+
+            if (ViewBag.user == null)
+            {
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Index", "Home", new { area = "", message = "Debes iniciar sesión" });
+            }
+            else if (ViewBag.connect == null)
+            {
+                return RedirectToAction("Logout", "Account", new { area = "", msg = "Debes elegir una empresa" });
+            }
+            else
+            {
+                Users user = Session["USER"] as Users;
+                string sucur = Session["BRANCH"].ToString();
+                int conn = int.Parse(Session["ID_CONN"].ToString());
+
+                ViewBag.data_conn = Session["DATA_CONN"].ToString();
+                ViewBag.bran_conn = Session["BRAN_CONN"].ToString();
+                ViewBag.username = user.Username;
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                serializer.MaxJsonLength = 50000000;
+
+                ViewBag.orders = serializer.Serialize(new BuyOrder().GetAllBuyOrders(200));
+
+                return View();
+            }
         }
     }
 }
---
+++ ProfitTM/Models/Admin/BuyOrder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ProfitTM.Models
+{
+    public class BuyOrder : ProfitAdmManager
+    {
+        public static saOrdenCompra GetPayOrderByID(string id)
+        {
+            saOrdenCompra buyOrder;
+
+            try
+            {
+                buyOrder = db.saOrdenCompra.AsNoTracking().Single(o => o.doc_num == id);
+            }
+            catch (Exception ex)
+            {
+                buyOrder = null;
+                Incident.CreateIncident("ERROR BUSCANDO ORDEN DE COMPRA " + id, ex);
+            }
+
+            return buyOrder;
+        }
+
+        public List<saOrdenCompra> GetAllBuyOrders(int number)
+        {
+            List<saOrdenCompra> orders;
+
+            try
+            {
+                orders = db.saOrdenCompra.AsNoTracking().OrderByDescending(i => i.fe_us_in).ThenBy(i => i.doc_num).Take(number).ToList();
+            }
+            catch (Exception ex)
+            {
+                orders = null;
+                Incident.CreateIncident("ERROR BUSCANDO ORDENES DE COMPRA", ex);
+            }
+
+            return orders;
+        }
+    }
+}
