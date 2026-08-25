c39101540cfedceb3028f02df5115ee6e78f345a
Alejandro <nestoraca.179@gmail.com>
2026-08-25 00:00:53 -0400

    fix: refactor trip number assignment to use GetTripNumber method for improved clarity

--- ProfitTM/Models/ATF/ModelsATF.cs
+++ ProfitTM/Models/ATF/ModelsATF.cs
@@ -224,7 +224,7 @@ namespace ProfitTM.Models
                     viajes = new Viajes()
                     {
                         razonSocialServTransporte = i.campo1,
-                        numeroBoleto = i.campo8,
+                        numeroBoleto = GetTripNumber(i.campo8), // i.campo8,
                         puntoSalida = i.campo2,
                         puntoDestino = i.descrip,
                     },
@@ -659,6 +659,14 @@ namespace ProfitTM.Models
                 emails.Add(c.campo3);
 
             return emails;
+        }
+
+        private string GetTripNumber(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return string.Empty;
+
+            return value.Replace("VELAG-", "");
         }
     }
 
